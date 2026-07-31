95f9d8e07979803edf6689a17c1d728f89eff6b3
Rahul Jain <rahuljait@gmail.com>
2026-08-01 00:12:32 +0530

    Add BMI calculation and score display

---
+++ day2-number-manipulation.py
@@ -0,0 +1,15 @@
+bmi = 84 / 1.65 ** 2
+print(bmi)
+print(int(bmi))
+
+print(round(bmi))
+
+print(round(bmi, 2))
+
+score =0
+#user scores a point
+# score+=1
+# print(score)
+
+print("Your score is", score)
+print(f"Your score is  = {bmi}")
